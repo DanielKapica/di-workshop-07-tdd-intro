d397b0f91b032fb94827446e1a17dc9b97ce183c
Daniel Kapica <kapica6105@gmail.com>
2018-10-24 14:09:23 +0100

    Part 2 Completed

--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -3,23 +3,23 @@ var expect = chai.expect
 
 var Coolculator = require('../lib/coolculator')
 
-describe('Coolculator', function() {
+describe('Coolculator', function () {
   mm = new Coolculator()
 
-  it('should add', function() {
+  it('should add', function () {
     result = mm.add(2, 3)
     expect(result).to.equal(5)
   })
 
   // Uncomment this 👇
-  // it('should multiply', function() {
-  //   result = mm.multiply(2, 3)
-  //   expect(result).to.equal(6)
-  // })
+  it('should multiply', function () {
+    result = mm.multiply(2, 3)
+    expect(result).to.equal(6)
+  })
 
   // Uncomment this 👇
-  // it('should subtract', function() {
-  //   result = mm.subtract(3, 2)
-  //   expect(result).to.equal(1)
-  // })
+  it('should subtract', function () {
+    result = mm.subtract(3, 2)
+    expect(result).to.equal(1)
+  })
 })
